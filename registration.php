c9461a765e1c48ccc7c5ebf554442fe7271248d9
Ariya InfoTech <toariyainfotech@gmail.com>
2022-06-03 13:13:12 +0530

    Module Add
    Module Update

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © ariya infotech All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'AriyaInfoTech_AutoUpdateCartAjax', __DIR__);
+
